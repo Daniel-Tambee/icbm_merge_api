0b76da5fb214eda99d0317d85487a400f0c18b9b
daniel tambee <daniel.tambee@zap.africa>
2025-06-27 11:35:05 +0100

    fix:new things

--- src/data_layer/frappe-prisma.service.ts
+++ src/data_layer/frappe-prisma.service.ts
@@ -9,7 +9,7 @@ export class FrappePrismaService extends PrismaClient {
   constructor() {
     super({
       datasourceUrl:
-        'mysql://root:6Gjfb2oKPK0OMwNO@localhost:3306/_d8e7a160b7badfc3',
+        'mysql://_d8e7a160b7badfc3:6Gjfb2oKPK0OMwNO@localhost:3306/_d8e7a160b7badfc3',
     });
   }
 }
